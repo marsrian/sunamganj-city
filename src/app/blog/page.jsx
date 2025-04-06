22b2233941013de9b5e29d6b95a89e256784a64c
marsrian <marsrian40@gmail.com>
2025-04-06 15:15:45 +0600

    blog card update ui

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -17,7 +17,7 @@ const getBlogData = async () => {
 const BlogPage = async () => {
   const blogs = await getBlogData();
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 max-w-7xl mx-auto">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 max-w-7xl mx-auto px-2 md:px-0">
       {blogs
         .filter((blog) => blog.approval === "approved")
         .map((blog) => (
